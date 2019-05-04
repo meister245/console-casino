0290ad91d07778b7821cfbd80938c6bef6dba6b6
Zsolt Mester <22896196+meister245@users.noreply.github.com>
2019-05-04 19:59:15 +0200

    added getRunningTime, added more strategies, fixes

--- src/js/roulette-bot.js
+++ src/js/roulette-bot.js
@@ -1,4 +1,4 @@
-import {waitingForResults} from "./constants";
+import {waitingForResults, winBet, loseBet} from "./constants";
 
 import {StreakDozens} from "./strategy/streak-dozens";
 import {StreakRedBlack} from "./strategy/streak-red-black";
@@ -10,21 +10,33 @@ class RouletteBot {
     constructor(stopLoss = 0) {
         this.active = true;
         this.stopLoss = stopLoss;
+        this.createTime = Math.floor(Date.now() / 1000);
+
         this.roulette = new PlaytechLiveRoulette();
 
         this.betStrategies = [
-            new StreakDozens(this.roulette),
-            new StreakRedBlack(this.roulette),
+            new StreakDozens(this.roulette, 2),
+            new StreakDozens(this.roulette, 3),
+            new StreakDozens(this.roulette, 4),
+            new StreakDozens(this.roulette, 5),
+            new StreakDozens(this.roulette, 6),
+            new StreakRedBlack(this.roulette, 2),
+            new StreakRedBlack(this.roulette, 3),
+            new StreakRedBlack(this.roulette, 4),
+            new StreakRedBlack(this.roulette, 5),
+            new StreakRedBlack(this.roulette, 6),
+            new StreakRedBlack(this.roulette, 7),
+            new StreakRedBlack(this.roulette, 8),
         ];
     }
 
     async start(chipSize = 0.1) {
         let ts = Date.now() / 1000;
-        let balance = await this.roulette.getBalance();
 
         while (this.active) {
             console.log('rouletteBot', 'running');
 
+            let balance = await this.roulette.getBalance();
             let numbers = await this.roulette.getLastNumbers();
             let dealerMessage = await this.roulette.getDealerMessage().toLowerCase();
 
@@ -34,7 +46,7 @@ class RouletteBot {
 
             for (let i = 0; i < this.betStrategies.length; i++) {
                 if (dealerMessage === 'place your bets' || dealerMessage === 'last bets') {
-                    balance += await this.betStrategies[i].processBet(numbers, balance, chipSize);
+                    await this.betStrategies[i].processBet(numbers, balance, chipSize);
 
                 } else {
                     this.betStrategies[i].betState = waitingForResults;
@@ -43,6 +55,11 @@ class RouletteBot {
             ts = await this.keepSessionAlive(ts);
             await sleep(3000);
         }
+    }
+
+    getRunningTime() {
+        let timeDiff = Math.floor(Date.now() / 1000) - this.createTime;
+        return Math.round(timeDiff / 60 / 60).toFixed(1) + ' hours';
     }
 
     getResults() {
@@ -55,16 +72,15 @@ class RouletteBot {
             let name = this.betStrategies[i].name;
 
             for (let s = 0; s < this.betStrategies[i].results.length; s++) {
-
                 sum += this.betStrategies[i].results[s].size;
             }
 
             for (let s = 0; s < this.betStrategies[i].results.length; s++) {
-                wins += (this.betStrategies[i].results[s].status === 'win') ? 1 : 0;
+                wins += (this.betStrategies[i].results[s].status === winBet) ? 1 : 0;
             }
 
             for (let s = 0; s < this.betStrategies[i].results.length; s++) {
-                losses += (this.betStrategies[i].results[s].status === 'lose') ? 1 : 0;
+                losses += (this.betStrategies[i].results[s].status === loseBet) ? 1 : 0;
             }
 
             data[name] = {
