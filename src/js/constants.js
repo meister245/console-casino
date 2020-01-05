b38103e66b3eb74bc15e9ca65b44c227ba56ebe3
Zsolt Mester <22896196+meister245@users.noreply.github.com>
2020-01-05 22:55:59 +0100

    updated constant names

--- src/js/constants.js
+++ src/js/constants.js
@@ -6,6 +6,6 @@ export const waitingForResults = 'waiting-for-results';
 export const winBet = 'win';
 export const loseBet = 'lose';
 
-export const redNumbers = [1, 3, 5, 7, 9, 12, 14, 16, 18, 19, 21, 23, 25, 27, 30, 32, 34, 36];
-export const blackNumbers = [2, 4, 6, 8, 10, 11, 13, 15, 17, 20, 22, 24, 26, 28, 29, 31, 33, 35];
+export const rouletteRedNumbers = [1, 3, 5, 7, 9, 12, 14, 16, 18, 19, 21, 23, 25, 27, 30, 32, 34, 36];
+export const rouletteBlackNumbers = [2, 4, 6, 8, 10, 11, 13, 15, 17, 20, 22, 24, 26, 28, 29, 31, 33, 35];
 
