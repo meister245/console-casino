10a6bc5f9fa9abf372b9a1ec8e09f7fbf3e3aa89
Zsolt Mester <22896196+meister245@users.noreply.github.com>
2020-01-08 23:24:00 +0100

    added getStrategies static method, removed try-catch block

--- src/js/bot/roulette-bot.js
+++ src/js/bot/roulette-bot.js
@@ -1,8 +1,8 @@
-import {ConsoleBot} from "./console-bot";
+import {CommonBot} from "./common";
 import {ProgressiveRedBlack} from "../strategy/progressive-red-black";
 
-class RouletteBot extends ConsoleBot {
-    constructor(driverName = '') {
+export class RouletteBot extends CommonBot {
+    constructor(driverName) {
         super();
         this.driver = this.getDriver(driverName);
     }
@@ -20,15 +20,9 @@ class RouletteBot extends ConsoleBot {
         await this.createTask(taskID, 'roulette', strategyName, Object.assign({}, strategy.results));
 
         while (this.getTask(taskID).active) {
-            try {
-                await strategy.runStrategy();
-                await this.updateTaskResults(taskID, Object.assign({}, strategy.results));
-                await this.sleep(1500);
-
-            } catch (e) {
-                await this.stop(taskID);
-                console.log(e);
-            }
+            await strategy.runStrategy();
+            await this.updateTaskResults(taskID, Object.assign({}, strategy.results));
+            await this.sleep(1500);
         }
     }
 
@@ -44,17 +38,23 @@ class RouletteBot extends ConsoleBot {
         } catch (e) {
             await this.driver.viewExtendedHistory();
             numbers = await this.driver.getExtendedHistory();
-        } finally {
-            strategy = await this.getStrategy('backtest', strategyName, bagSize, options);
-            await strategy.runBacktest(numbers);
         }
+
+        strategy = await this.getStrategy('backtest', strategyName, bagSize, options);
+        await strategy.runBacktest(numbers);
     }
 
     getStrategy(taskID, strategyName, bagSize, options) {
+        if (!(RouletteBot.getStrategies().includes(strategyName))) {
+            throw new Error('invalid strategy name ' + strategyName);
+        }
+
         if (strategyName === 'progressive-red-black') {
             return new ProgressiveRedBlack(this.driver, taskID, bagSize, this.getOptions(options));
         }
+    }
 
-        throw new Error('strategy not found: ' + strategyName);
+    static getStrategies() {
+        return ['progressive-red-black']
     }
 }
