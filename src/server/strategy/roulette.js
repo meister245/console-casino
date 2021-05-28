e3a6246578900935ced0a55054241cfdf04a7b10
Zsolt Mester <22896196+meister245@users.noreply.github.com>
2021-05-28 10:06:13 +0200

    revised strategies

--- src/server/strategy/roulette.js
+++ src/server/strategy/roulette.js
@@ -52,5 +52,59 @@ module.exports = {
       pattern: ['low', 'low'],
       distribution: ['high', 100, 40, 'lowerEqual']
     }
+  },
+  dozenFirstHighOccurrence: {
+    bets: ['dozenSecond', 'dozenThird'],
+    progressionMultiplier: 3,
+    stopLossLimit: 5,
+    trigger: {
+      pattern: ['dozenFirst', 'dozenFirst', 'dozenFirst', 'dozenFirst'],
+      distribution: ['dozenFirst', 100, 36, 'higherEqual']
+    }
+  },
+  dozenSecondHighOccurrence: {
+    bets: ['dozenFirst', 'dozenThird'],
+    progressionMultiplier: 3,
+    stopLossLimit: 5,
+    trigger: {
+      pattern: ['dozenSecond', 'dozenSecond', 'dozenSecond', 'dozenSecond'],
+      distribution: ['dozenSecond', 100, 36, 'higherEqual']
+    }
+  },
+  dozenThirdHighOccurrence: {
+    bets: ['dozenFirst', 'dozenSecond'],
+    progressionMultiplier: 3,
+    betLossLimit: 5,
+    trigger: {
+      pattern: ['dozenThird', 'dozenThird', 'dozenThird', 'dozenThird'],
+      distribution: ['dozenThird', 100, 36, 'higherEqual']
+    }
+  },
+  columnTopHighOccurrence: {
+    bets: ['columnMiddle', 'columnBottom'],
+    progressionMultiplier: 3,
+    stopLossLimit: 5,
+    trigger: {
+      pattern: ['columnTop', 'columnTop', 'columnTop', 'columnTop'],
+      distribution: ['columnTop', 100, 36, 'higherEqual']
+    }
+  },
+  columnMiddleHighOccurrence: {
+    bets: ['columnTop', 'columnBottom'],
+    progressionMultiplier: 3,
+    stopLossLimit: 5,
+    trigger: {
+      pattern: ['columnMiddle', 'columnMiddle', 'columnMiddle', 'columnMiddle'],
+      distribution: ['columnMiddle', 100, 36, 'higherEqual']
+    }
+  },
+  columnBottomHighOccurrence: {
+    bets: ['columnTop', 'columnMiddle'],
+    progressionMultiplier: 3,
+    stopLossLimit: 5,
+    trigger: {
+      pattern: ['columnBottom', 'columnBottom', 'columnBottom', 'columnBottom'],
+      distribution: ['columnBottom', 100, 36, 'higherEqual']
+    }
   }
 }
